86d2edb3aa3d2413e8dc26ffd30fd9ee2a69b5c6
Austin Kohler <austinckohler@gmail.com>
2020-02-25 22:08:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(lib)
 library = YAML.load_file(lib)
 result = {"get_meaning" => {}, "get_emoticon" => {}}
-library do |m, e| 
+library.each do |m, e| 
   result["get_meaning"][e[1]] = m
   result["get_emoticon"][m[0]] = e[1]
 end
