cb7f27df937665f32b935fe02bc5329befd0b538
Austin Kohler <austinckohler@gmail.com>
2020-02-25 22:19:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,8 @@ end
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', emoticon)
   library = load_library(lib)
   if library["get_emoticon"]include?(emoticon)
-    library["get_emoticon"]
+    library["get_emoticon"][emoticon]
+  e
   # code goes here
 end
 
