4ee27b2d5cb9857d916834988c47a84c92ff800d
Austin Kohler <austinckohler@gmail.com>
2020-02-25 21:35:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ def load_library(lib)
 library = YAML.load_file(lib)
 result = {"get_meaning" => {}, "get_emoticon" => {}}
 library do |m, e| 
-  result["get_meaning"][e[1]] = 
+  result["get_meaning"][e[1]] = m
+  result["get_emoticon"][m[0]] =
 end
 
 def get_japanese_emoticon
