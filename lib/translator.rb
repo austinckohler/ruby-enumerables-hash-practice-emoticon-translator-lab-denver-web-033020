0d6cd0d13b146dfb57f2fb208888043b9eac20a8
Austin Kohler <austinckohler@gmail.com>
2020-02-25 22:33:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,6 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', emoticon)
 end
 end
 
-def get_english_meaning()
+def get_english_meaning(file_path = './lib/emoticons.yml', emoticon)
   # code goes here
 end
