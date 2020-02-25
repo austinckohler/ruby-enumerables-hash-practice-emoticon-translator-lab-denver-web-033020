779df909ade79dafe8b48b42e5b5cd56da81d6ac
Austin Kohler <austinckohler@gmail.com>
2020-02-25 22:31:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', emoticon)
   if library["get_emoticon"].include?(emoticon)
     library["get_emoticon"][emoticon]
   else 
-  puts "Sorry, emoticon not found" 
+  "Sorry, that emoticon was not found" 
 end
 end
 
